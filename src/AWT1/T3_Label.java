0434ee5666761288435f3d974a025ba04e3339a1
haneulkg <kgksmf22@naver.com>
2023-09-19 17:56:56 +0900

    20230919_1755 AWT 연습

---
+++ src/AWT1/T3_Label.java
@@ -0,0 +1,54 @@
+package AWT1;
+
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+@SuppressWarnings("serial")
+public class T3_Label extends Frame implements WindowListener {
+	
+	public T3_Label() {
+		// 디자인
+		super("프레임 테스트");
+		setSize(400, 300);
+		
+		Label lbl = new Label("회 원 가 입 폼");
+		
+		this.add(lbl);
+		
+		setVisible(true);
+		
+		/* ----------------------------------- */
+		// 코드
+		addWindowListener(this);
+	}
+	
+	public static void main(String[] args) {
+		new T3_Label();
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {}
+
+	@Override
+	public void windowIconified(WindowEvent e) {}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {}
+
+	@Override
+	public void windowActivated(WindowEvent e) {}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {}
+	
+}
